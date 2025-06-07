08bd37b474139163b029c1e74811bd3b877de7be
pixilate <116592006+happicatalpa@users.noreply.github.com>
2025-06-06 19:49:14 -0700

    added HDRI and flowers and hive

---
+++ Assets/Scripts/InteractPrompt.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InteractPrompt : MonoBehaviour
+{
+    public GameObject interactPrompt; // The TMP message
+    public Transform player; // Track position of player
+    public float interactionRange = 3f; // Set max range for interaction
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(player.position, transform.position); // Get distance between interactable object and player
+
+        if (distance < interactionRange)
+        {
+            interactPrompt.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("entered the hive!");
+                getObjectInteraction();
+            }
+
+        } 
+        
+        else
+        {
+            interactPrompt.SetActive(false);
+        }
+
+        Debug.Log("Distance: " + distance);
+
+    }
+
+    private void getObjectInteraction ()
+    {
+        if (tag == "Beehive")
+        {
+            SceneManager.LoadScene("Beehive");
+        }
+    }
+
+}
